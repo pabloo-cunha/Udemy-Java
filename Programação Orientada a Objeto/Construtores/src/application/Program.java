ef0ecd824f5f800c14849e89c1bec7c669a1c2c8
pabloo-cunha <pablo.limacunha@outlook.com>
2023-02-06 17:58:20 -0300

    aplicando getter and setter

--- Programação Orientada a Objeto/Construtores/src/application/Program.java
+++ Programação Orientada a Objeto/Construtores/src/application/Program.java
@@ -26,6 +26,11 @@ public class Program {
 
         Product product = new Product(name, price);
 
+        product.setName("Computer");
+        System.out.println("Update name: " + product.getName());
+        product.setPrice(1200.00);
+        System.out.println("Update price: " + product.getPrice());
+
         System.out.println("Product data: " + product);
         System.out.println();
 
