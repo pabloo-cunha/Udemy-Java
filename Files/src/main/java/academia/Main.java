083944d37cb26572b6b6825d1fe49088c1f51fdb
pabloo-cunha <pablo.limacunha@outlook.com>
2023-06-01 12:50:40 -0300

    Trabalhando com Files

--- Files/src/main/java/academia/Main.java
+++ Files/src/main/java/academia/Main.java
@@ -1,7 +1,26 @@
 package academia;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        File file = new File("C:\\Users\\pablo\\Documents\\Programas\\Cursos\\Udemy-Java\\Files\\arquivos\\in.txt");
+        Scanner sc = null;
+        try{
+            sc = new Scanner(file);
+            while (sc.hasNext()){
+                System.out.println(sc.nextLine());
+            }
+        }
+        catch (IOException e){
+            System.out.println("Error: " + e.getMessage());
+        }
+        finally {
+            if (sc != null)
+                sc.close();
+        }
     }
 }
