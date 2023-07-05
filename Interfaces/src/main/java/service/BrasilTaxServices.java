2ceb9396702ee4f4c5d3423aa080407c15542e0d
pabloo-cunha <pablo.limacunha@outlook.com>
2023-07-05 13:20:17 -0300

    exercicio de interface

---
+++ Interfaces/src/main/java/service/BrasilTaxServices.java
@@ -0,0 +1,13 @@
+package service;
+
+public class BrasilTaxServices implements TaxService {
+
+    @Override
+    public Double tax(Double amount) {
+        if (amount <= 100){
+            return amount * 0.2;
+        }
+        else
+            return amount *0.15;
+    }
+}
